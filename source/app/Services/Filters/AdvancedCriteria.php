4a9f57908bc2489bbcc60e38873565364cc998de
Khaled Sabbah <khaledgamalsabbah@gmail.com>
2018-11-05 20:52:20 +0200

    adding advanced criteria to implemented sooner

---
+++ source/app/Services/Filters/AdvancedCriteria.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khaledgamal
+ * Date: 11/5/18
+ * Time: 8:16 PM
+ */
+
+namespace App\Services\Filters;
+
+
+use App\Contracts\ICriteria;
+
+class AdvancedCriteria implements ICriteria
+{
+
+    public function meetCriteria(\DOMNodeList $domNodeList): array
+    {
+        // TODO: Implement meetCriteria() method.
+    }
+}
